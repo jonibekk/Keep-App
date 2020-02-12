f8d22ba489d701204560c1a5d05f5a531350c75d
Jonibek <jonibeknorboev@gmail.com>
2020-02-12 17:23:56 +0900

    Updates

---
+++ precache-manifest.987e66af896e2f749e43913c1c56f523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720421027688cdab22a987fa313fc2db",
+    "url": "/my_keep_app/index.html"
+  },
+  {
+    "revision": "7435df1b25f6faaf5422",
+    "url": "/my_keep_app/static/css/main.9f21ff41.chunk.css"
+  },
+  {
+    "revision": "93522858d6e772393bee",
+    "url": "/my_keep_app/static/js/2.563f4096.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my_keep_app/static/js/2.563f4096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7435df1b25f6faaf5422",
+    "url": "/my_keep_app/static/js/main.e619e9ad.chunk.js"
+  },
+  {
+    "revision": "2a5197d6924c78fd8f34",
+    "url": "/my_keep_app/static/js/runtime-main.f47cec13.js"
+  }
+]);
